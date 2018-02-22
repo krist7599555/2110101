df5a26dbe36a0985f1d693c577b7cd33f965f6d9
Krist Pornpairin <krist_7599555@hotmail.com>
2018-02-22 20:22:41 +0700

    Update solve.py

--- mock_quiz/solve.py
+++ mock_quiz/solve.py
@@ -1,40 +1,19 @@
 def solve_1(n):
-    assert isinstance(n, int) and n >= 0
-    print('Test :', n)
     while n > 1:
         if n & 1:
             print(n)
             n = n * 3 + 1
-            print(n)
-            n //= 2
-        else:
-            print(n)
-            n //= 2
+        print(n)
+        n //= 2
     print('1' if n else '0!')
-solve_1(0)
-solve_1(1)
-solve_1(2)
-solve_1(3)
-solve_1(4)
-solve_1(7)
-solve_1(19)
-solve_1(10)
+
 def solve_2(s):
     for i in range(len(s)):
-        for j in range(len(s)):
-            if i < j and \
-               s[i].islower() and \
-               s[j].islower() and \
-               s[j] < s[i]:
+        for j in range(i + 1, len(s)):
+            if 'a' <= s[j] < s[i] <= 'z':
                 s = s[:i] + s[j] + s[i+1:j] + s[i] + s[j+1:]
     print(s)
-solve_2('bca')
-solve_2('bcaBAC')
-solve_2('Abb12BcaC123')
-solve_2('')
-solve_2('1c2a34c56c78c9b')
-solve_2('AaBbbCCCcDef12435ADVBFadsfbBRFRWb')
-solve_2('A aBb b C CC!ncD **e)f12m435ADVB# @@Fad!! sf()(bBRFRWb')
+
 def solve_3(n, k):
     if n <= 0 or k < 0 or k > n:
         print('empty')
@@ -50,6 +29,22 @@ def solve_3(n, k):
         o = s[i: i+n]
         if o.count('1') == k:
             print(o)
+
+solve_1(0)
+solve_1(1)
+solve_1(2)
+solve_1(3)
+solve_1(4)
+solve_1(7)
+solve_1(19)
+solve_1(10)
+solve_2('bca')
+solve_2('bcaBAC')
+solve_2('Abb12BcaC123')
+solve_2('')
+solve_2('1c2a34c56c78c9b')
+solve_2('AaBbbCCCcDef12435ADVBFadsfbBRFRWb')
+solve_2('A aBb b C CC!ncD **e)f12m435ADVB# @@Fad!! sf()(bBRFRWb')
 solve_3(0, 0)
 solve_3(0, 7)
 solve_3(1, 7)
